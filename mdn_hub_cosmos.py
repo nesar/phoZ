724434511db561c4d572f8623abca221dbc27c79
nesar <nesar@github.com>
2019-12-20 11:59:43 -0500

    new sdss cosmos runs

--- mdn_hub_cosmos.py
+++ mdn_hub_cosmos.py
@@ -549,7 +549,7 @@ def plot_cum_sigma(pred_weights,pred_std,ymax,ymin):
 
 n_epochs = 100002 #20000 #100000 #1000 #20000 #20000
 # N = 4000  # number of data points  -- replaced by num_trai
-D = 14 #6  # number of features  (8 for DES, 6 for COSMOS)
+D = 15 #6  # number of features  (8 for DES, 6 for COSMOS)
 K = 3 # number of mixture components
 
 
--- mdn_hub_sdss.py
+++ mdn_hub_sdss.py
@@ -548,7 +548,7 @@ def plot_cum_sigma(pred_weights,pred_std,ymax,ymin):
 
 n_epochs = 100002 #000 #20000 #100000 #1000 #20000 #20000
 # N = 4000  # number of data points  -- replaced by num_trai
-D = 3 #6  # number of features  (8 for DES, 6 for COSMOS)
+D = 4 #6  # number of features  (8 for DES, 6 for COSMOS)
 K = 3 # number of mixture components
 
 
