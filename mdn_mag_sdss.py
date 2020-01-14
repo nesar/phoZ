d069ff3d912d6e50cf032918996d4d20611cdb97
nesar <nesar@github.com>
2020-01-14 16:31:32 -0500

    new runs sdss cosmos ms

--- mdn_mag_sdss.py
+++ mdn_mag_sdss.py
@@ -568,7 +568,7 @@ def plot_cum_sigma(pred_weights,pred_std,ymax,ymin):
 
 
 
-n_epochs = 10 #000 #20000 #100000 #1000 #20000 #20000
+n_epochs = 100015 #000 #20000 #100000 #1000 #20000 #20000
 # N = 4000  # number of data points  -- replaced by num_trai
 D = 5 #6  # number of features  (8 for DES, 6 for COSMOS)
 K = 3 # number of mixture components
