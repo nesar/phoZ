577e1ff39d664f142ed55b9c390dba6421584afe
nesar <nesar@github.com>
2020-01-14 15:30:49 -0500

    obs training

--- mdn_hub2.py
+++ mdn_hub2.py
@@ -481,7 +481,7 @@ def plot_cum_sigma(pred_weights,pred_std,ymax,ymin):
 
 
 
-n_epochs = 40000 #100000 #1000 #20000 #20000
+n_epochs = 80000 #20000 #100000 #1000 #20000 #20000
 # N = 4000  # number of data points  -- replaced by num_trai
 D = 5 #6  # number of features  (8 for DES, 6 for COSMOS)
 K = 3 # number of mixture components
@@ -489,7 +489,7 @@ K = 3 # number of mixture components
 
 learning_rate = 1e-4 #5e-3
 decay_rate= 0.01 #0.0
-step=100
+step=1000 #100
 
 
 num_train = 800000 #12000000 #800000
